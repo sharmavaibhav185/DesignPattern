9a73b848f49fb3206b397f08b20a2221bcf06cd8
Vaibhav <sharmavaibhav185@gmail.com>
2023-05-21 17:59:45 +0530

    Singleton code added and this is thread safe

--- src/main/java/com/system/Creational/Singleton.java
+++ src/main/java/com/system/Creational/Singleton.java
@@ -1,4 +1,26 @@
 package com.system.Creational;
 
 public class Singleton {
+
+    private static Singleton INSTANCE = null;
+
+    private Singleton(){}
+
+    public static Singleton getInstance(){
+        if(INSTANCE==null){
+            synchronized (Singleton.class){
+                if (INSTANCE == null) {
+                    try{
+                        Thread.sleep(1000);
+                    }
+                    catch (InterruptedException ie){
+                        System.out.println("Exception occured"+ ie.getMessage());
+                    }
+                    INSTANCE=new Singleton();
+                }
+            }
+        }
+        return INSTANCE;
+    }
+
 }
